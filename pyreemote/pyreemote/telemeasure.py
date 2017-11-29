6849ba154c21b27e7e810a8660f6372574da3776
MiquelIR <misernroca@gisce.net>
2017-11-29 09:21:04 +0100

    NEW Function to validate the dates format

--- pyreemote/pyreemote/telemeasure.py
+++ pyreemote/pyreemote/telemeasure.py
@@ -2,22 +2,43 @@
 
 from subprocess import Popen, PIPE
 import json
+import logging
+from datetime import datetime
+
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='[%Y-%m-%dT%H:%M:%S]',
+                    level=logging.INFO)
+
+
+def validate(date_text):
+    try:
+        if date_text != datetime.strptime(date_text, '%Y-%m-%dT%H:%M:%S'
+                                          ).strftime('%Y-%m-%dT%H:%M:%S'):
+            raise ValueError
+        return True
+    except ValueError:
+        return False
 
 
 class ReemoteWrapper(object):
 
     def __init__(self, ipaddr, port, link, mpoint, passwrd, datefrom, dateto,
                  option, request, contract):
-        self.ipaddr = ipaddr
-        self.port = port
-        self.link = link
-        self.mpoint = mpoint
-        self.passwrd = passwrd
-        self.datefrom = datefrom
-        self.dateto = dateto
-        self.option = option
-        self.request = request
-        self.contract = contract
+        if validate(datefrom) and validate(dateto):
+            self.ipaddr = ipaddr
+            self.port = port
+            self.link = link
+            self.mpoint = mpoint
+            self.passwrd = passwrd
+            self.datefrom = datefrom
+            self.dateto = dateto
+            self.option = option
+            self.request = request
+            self.contract = contract
+        else:
+            logging.getLogger(__name__).info(
+                'ERROR: Date format is wrong. Expected: %Y-%m-%dT%H:%M:%S'
+            )
     
     def execute_request(self):
         command = "mono reemote.exe -i {0} -o {1} -l {2} -m {3} -w {4} " \
