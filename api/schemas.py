e61de2a38bc8f523f97f1e5bc07bfae5d6e17a85
Xavi Torelló <info@xaviertorello.cat>
2018-02-12 18:23:07 +0100

    Add CallSchema schema with main fields

--- api/schemas.py
+++ api/schemas.py
@@ -1,2 +1,16 @@
 # -*- coding: utf-8 -*-
 from marshmallow import Schema, fields
+
+class CallSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    
+    port = fields.Int()
+    link_address = fields.Int()
+    mpoint = fields.Int()
+    password = fields.Int()
+    date_from = fields.String()
+    date_to = fields.String()
+    option = fields.Str()
+    request = fields.String()
+    contract = fields.List(fields.Int)
