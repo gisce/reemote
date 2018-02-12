00b5bcf0b840ea3e73bbbbe56a50ca7302d7f5f2
Xavi Torelló <info@xaviertorello.cat>
2018-02-12 18:34:00 +0100

    Assert that call status can be fetched

--- api/specs/api_spec.py
+++ api/specs/api_spec.py
@@ -68,3 +68,8 @@ with description('API'):
 
                 job_id = result['id']
                 assert job_id, "Job ID must be fetched"
+
+                # Second part, fetch the status of enqueued ID
+                self.url = base_url + "/call/{}".format(job_id)
+                response = self.app.get(self.url, follow_redirects=True)
+                assert response.status_code == 200
