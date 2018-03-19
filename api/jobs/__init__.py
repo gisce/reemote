04d950d5492d083c9dc46652566090114a3f95b7
Xavi Torelló <info@xaviertorello.cat>
2018-03-19 12:49:29 +0100

    Return the execution of the Wrapper

--- api/jobs/__init__.py
+++ api/jobs/__init__.py
@@ -4,4 +4,4 @@ def call_using_custom_wrapper(wrapper, params):
     print ("Starting a new job")
     print (wrapper)
     print (params)
-    wrapper(**params)
+    return wrapper(**params).execute_request()
