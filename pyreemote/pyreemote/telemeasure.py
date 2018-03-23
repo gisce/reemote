8fcd5cc01326fd5688fa219c08de41a9c5d77463
MiquelIR <misernroca@gisce.net>
2018-03-23 12:59:58 +0100

    FIX code and ADD get_status method

--- pyreemote/pyreemote/telemeasure.py
+++ pyreemote/pyreemote/telemeasure.py
@@ -112,7 +112,7 @@ class ReemoteTCPIPWrapper(object):
             post_data = {
                 'ipaddr': self.ipaddr,
                 'port': self.port,
-                'link_address': self.link,
+                'link': self.link,
                 'mpoint': self.mpoint,
                 'passwrd': self.passwrd,
                 'datefrom': self.datefrom,
@@ -121,7 +121,8 @@ class ReemoteTCPIPWrapper(object):
                 'request': self.request,
                 'contract': self.contract
             }
-            response = requests.post(self.reemote.geturl(), data=post_data)
+            response = requests.post(self.reemote.geturl(), data=post_data, allow_redirects=True)
+            response = json.loads(response.content)
             if response['error']:
                 result = {
                     'error': True,
@@ -143,6 +144,10 @@ class ReemoteTCPIPWrapper(object):
 
         return result
 
+    def get_status(self, job_id):
+        url = self.reemote.geturl() + "{}".format(job_id)
+        response = requests.get(url)
+        return json.loads(response.content)
 
 class ReemoteModemWrapper(object):
 
